0c1202c345cae1f338f933c25e03efcc40b58e8f
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2020-07-30 15:47:07 +0100

    some basic map thingy working for task

--- bpf_camflow_kern.c
+++ bpf_camflow_kern.c
@@ -17,6 +17,13 @@ struct bpf_map_def SEC("maps") my_map = {
         .max_entries = 1,
 };
 
+struct bpf_map_def SEC("maps") task_map = {
+        .type = BPF_MAP_TYPE_ARRAY,
+        .key_size = sizeof(u32), // probably wants to change
+        .value_size = sizeof(struct task_prov_struct),
+        .max_entries = 4096, // how to setup the size? is there as big as needed option?
+};
+
 static __always_inline void count(void *map)
 {
 	u32 key = 0;
@@ -34,6 +41,18 @@ static __always_inline void count(void *map)
 SEC("lsm/task_alloc")
 int BPF_PROG(task_alloc, struct task_struct *task, unsigned long clone_flags)
 {
+  uint32_t pid  = task->pid;
+  /* it needs to be initialised */
+  struct task_prov_struct prov = {.pid = task->pid};
+  bpf_map_update_elem(&task_map, &pid, &prov, BPF_NOEXIST);
   count(&my_map);
 	return 0;
+}
+
+SEC("lsm/task_free")
+int BPF_PROG(task_free, struct task_struct *task)
+{
+  uint32_t pid  = 0;
+  bpf_map_delete_elem(&task_map, &pid);
+  return 0;
 }
