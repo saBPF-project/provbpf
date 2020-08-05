31aba8042522db922127c45e6d35864183e38f7e
Michael Han <hanx@g.harvard.edu>
2020-08-05 15:33:49 +0000

    Clean up a bit and format code nicely for sanity

--- bpf_camflow_kern.c
+++ bpf_camflow_kern.c
@@ -10,49 +10,24 @@
 
 char _license[] SEC("license") = "GPL";
 
-struct bpf_map_def SEC("maps") my_map = {
-        .type = BPF_MAP_TYPE_ARRAY,
-        .key_size = sizeof(uint32_t),
-        .value_size = sizeof(uint32_t),
-        .max_entries = 1,
-};
-
 struct bpf_map_def SEC("maps") task_map = {
-        .type = BPF_MAP_TYPE_HASH,
-        .key_size = sizeof(uint32_t), // probably wants to change
-        .value_size = sizeof(struct task_prov_struct),
-        .max_entries = 4096, // how to setup the size? is there as big as needed option?
+    .type = BPF_MAP_TYPE_HASH,
+    .key_size = sizeof(uint32_t),
+    .value_size = sizeof(struct task_prov_struct),
+    .max_entries = 4096, // TODO: dynamically increase size if possible
 };
-
-static __always_inline void count(void *map)
-{
-	uint32_t key = 0;
-	uint32_t *value, init_val = 1;
-
-  // retrieve value of element 0
-	value = bpf_map_lookup_elem(map, &key);
-  // increment if exists, otherwise insert
-	if (value)
-		*value += 1;
-	else
-		bpf_map_update_elem(map, &key, &init_val, BPF_NOEXIST);
-}
 
 SEC("lsm/task_alloc")
-int BPF_PROG(task_alloc, struct task_struct *task, unsigned long clone_flags)
-{
-  uint32_t pid  = task->pid;
-  /* it needs to be initialised */
-  struct task_prov_struct prov = {.pid = task->pid};
-  bpf_map_update_elem(&task_map, &pid, &prov, BPF_NOEXIST);
-  count(&my_map);
-	return 0;
+int BPF_PROG(task_alloc, struct task_struct *task, unsigned long clone_flags) {
+    uint32_t pid  = task->pid;
+    struct task_prov_struct prov = {.pid = task->pid};
+    bpf_map_update_elem(&task_map, &pid, &prov, BPF_NOEXIST);
+    return 0;
 }
 
 SEC("lsm/task_free")
-int BPF_PROG(task_free, struct task_struct *task)
-{
-  uint32_t pid  = 0;
-  bpf_map_delete_elem(&task_map, &pid);
-  return 0;
+int BPF_PROG(task_free, struct task_struct *task) {
+    uint32_t pid  = 0;
+    bpf_map_delete_elem(&task_map, &pid);
+    return 0;
 }
--- bpf_camflow_usr.c
+++ bpf_camflow_usr.c
@@ -6,40 +6,34 @@
 
 #include "bpf_camflow.skel.h"
 
-int main(void)
-{
-	struct bpf_camflow_kern *skel = NULL;
-  int err;
-	int map_fd;
-	unsigned int key = 0, value;
+int main(void) {
+    struct bpf_camflow_kern *skel = NULL;
+    int err;
+    int map_fd;
+    unsigned int key = 0, value;
+    
+    printf("Starting...\n");
 
-  printf("Starting...\n");
-
-	skel = bpf_camflow_kern__open_and_load();
-	if (!skel) {
-    printf("Failed loading ...\n");
-		printf("LIBBPF_ERRNO__PROGTYPE: %d\n", LIBBPF_ERRNO__PROGTYPE);
-		printf("Kernel doesn't support this program type.\n");
-		goto close_prog;
-  }
-
-
-	err = bpf_camflow_kern__attach(skel);
-	if (err) {
-    printf("Failed attach ... %d\n", err);
-    goto close_prog;
-  }
-	map_fd = bpf_object__find_map_fd_by_name(skel->obj, "my_map");
-
-	err = bpf_map_lookup_elem(map_fd, &key, &value);
-	printf("err: %d value: %d\n", err, value);
-  printf("Sleeping...\n");
-  sleep(20);
-  printf("Slept.\n");
-	err = bpf_map_lookup_elem(map_fd, &key, &value);
-	printf("err: %d value: %d\n", err, value);
+    skel = bpf_camflow_kern__open_and_load();
+    if (!skel) {
+        printf("Failed loading ...\n");
+	printf("LIBBPF_ERRNO__PROGTYPE: %d\n", LIBBPF_ERRNO__PROGTYPE);
+	printf("Kernel doesn't support this program type.\n");
+	goto close_prog;
+    }
+    
+    err = bpf_camflow_kern__attach(skel);
+    if (err) {
+        printf("Failed attach ... %d\n", err);
+	goto close_prog;
+    }
+    
+    map_fd = bpf_object__find_map_fd_by_name(skel->obj, "task_map");
+    //err = bpf_map_lookup_elem(map_fd, &key, &value);
+    //printf("err: %d value: %d\n", err, value);
 
 close_prog:
-	bpf_camflow_kern__destroy(skel);
-  return 0;
+    bpf_camflow_kern__destroy(skel);
+    
+    return 0;
 }
