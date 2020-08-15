1c641f36d3832dc13801665b3f0528c9cff0f97c
Michael Han <hanx@g.harvard.edu>
2020-08-15 20:28:10 +0000

    Simple example using ring buffer to store pid only. Seems to work but child pid is not recorded.

--- bpf_camflow_kern.c
+++ bpf_camflow_kern.c
@@ -10,11 +10,14 @@
 
 char _license[] SEC("license") = "GPL";
 
-// NOTE: example reference:
+// NOTE: ring buffer reference:
 // https://elixir.bootlin.com/linux/v5.8/source/tools/testing/selftests/bpf/progs/test_ringbuf.c
 struct bpf_map_def SEC("maps") r_buf = {
     .type = BPF_MAP_TYPE_RINGBUF,
-    .max_entries = 4096 * 64, // NOTE: size seems to matter. previous value not ok
+    /* NOTE: The minimum size seems to be 1 << 12.
+     * Any value smaller than this results in
+     * runtime error. */
+    .max_entries = 1 << 12,
 };
 
 struct bpf_map_def SEC("maps") task_map = {
@@ -24,13 +27,26 @@ struct bpf_map_def SEC("maps") task_map = {
     .max_entries = 4096, // NOTE: set as big as possible; real size is dynamically adjusted
 };
 
+struct entry {
+    uint32_t pid;
+};
+
 SEC("lsm/task_alloc")
 int BPF_PROG(task_alloc, struct task_struct *task, unsigned long clone_flags) {
+    struct entry *prov_entry;
     uint32_t pid  = task->pid;
     struct task_prov_struct prov = {.pid = task->pid};
     bpf_map_update_elem(&task_map, &pid, &prov, BPF_NOEXIST);
-    // Add an entry to the ring buffer (not tested)
-    bpf_ringbuf_output(&r_buf, &pid, sizeof(uint32_t), 0);
+    /* Add an entry to the ring buffer by first
+     * reserving some space in the ring buffer. */
+    prov_entry = bpf_ringbuf_reserve(&r_buf, sizeof(*prov_entry), 0);
+    /* Reserving space failed. */
+    if (!prov_entry)
+        return 1;
+    /* Populate the entry with data. */
+    prov_entry->pid = pid;
+    /* prov_entry is ready to be committed. */
+    bpf_ringbuf_submit(prov_entry, 0);
     return 0;
 }
 
--- bpf_camflow_usr.c
+++ bpf_camflow_usr.c
@@ -6,52 +6,81 @@
 
 #include "bpf_camflow.skel.h"
 
-static int buf_process_entry(void *ctx, void *data, size_t len)
-{
-  printf("Read data of size %zu\n", len);
-	return 0;
+struct entry {
+    uint32_t pid;
+};
+
+/* Callback function for ring buffer. */
+static int buf_process_entry(void *ctx, void *data, size_t len) {
+    struct entry *record;
+    printf("Reading data of size %zu from the ring buffer...\n", len);
+    /* data should be the ring buffer entry. */
+    record = (struct entry*)data;
+    printf("New pid from ring buffer: %u\n", record->pid);
+    return 0;
 }
 
 int main(void) {
-  struct bpf_camflow_kern *skel = NULL;
-  struct ring_buffer *ringbuf = NULL;
-  int err;
-  int map_fd;
-  unsigned int key = 0, value;
+    struct bpf_camflow_kern *skel = NULL;
+    struct ring_buffer *ringbuf = NULL;
+    int err, map_fd;
+    pid_t pid;
+    unsigned int key = 0, value;
+    
+    printf("Starting...\n");
+    
+    skel = bpf_camflow_kern__open_and_load();
+    if (!skel) {
+        printf("Failed loading ...\n");
+	printf("Kernel doesn't support this program type.\n");
+	goto close_prog;
+    }
+    
+    err = bpf_camflow_kern__attach(skel);
+    if (err) {
+        printf("Failed attach ... %d\n", err);
+	goto close_prog;
+    }
+    
+    //map_fd = bpf_object__find_map_fd_by_name(skel->obj, "task_map");
+    //err = bpf_map_lookup_elem(map_fd, &key, &value);
+    //printf("err: %d value: %d\n", err, value);
 
-  printf("Starting...\n");
+    // Consume data from ring buffer
+    // Reference: https://elixir.bootlin.com/linux/v5.8/source/tools/testing/selftests/bpf/benchs/bench_ringbufs.c
+    printf("Locating the ring buffer...\n");
+    map_fd = bpf_object__find_map_fd_by_name(skel->obj, "r_buf");
+    if (map_fd < 0) {
+        printf("Failed to load the ring buffer... %d\n", map_fd);
+	goto close_prog;
+    }
+    
+    /* Create a new ring buffer handle in the userspace.
+     * buf_process_entry is the callback function that
+     * process the entry in the ring buffer. */
+    ringbuf = ring_buffer__new(map_fd, buf_process_entry, NULL, NULL);
 
-  skel = bpf_camflow_kern__open_and_load();
-  if (!skel) {
-    printf("Failed loading ...\n");
-    printf("Kernel doesn't support this program type.\n");
-    goto close_prog;
-  }
-
-  err = bpf_camflow_kern__attach(skel);
-  if (err) {
-    printf("Failed attach ... %d\n", err);
-    goto close_prog;
-  }
-
-  //map_fd = bpf_object__find_map_fd_by_name(skel->obj, "task_map");
-  //err = bpf_map_lookup_elem(map_fd, &key, &value);
-  //printf("err: %d value: %d\n", err, value);
-
-  // Consume data from ring buffer (not tested)
-  // Reference: https://elixir.bootlin.com/linux/v5.8/source/tools/testing/selftests/bpf/benchs/bench_ringbufs.c
-  printf("Locating map...\n");
-  map_fd = bpf_object__find_map_fd_by_name(skel->obj, "r_buf");
-  if (map_fd < 0) {
-    printf("Failed loading map ... %d\n", map_fd);
-    goto close_prog;
-  }
-  printf("Not sure what that does... (Michael?)\n");
-  ringbuf = ring_buffer__new(map_fd, buf_process_entry, NULL, NULL);
-  printf("Polling...\n");
-  while (ring_buffer__poll(ringbuf, -1) >= 0);
+    /* Check which pid is being captured by the task_alloc hook. */
+    pid = fork();
+    if (pid == 0) {
+        sleep(5);
+        printf("Child process %u is running...\n", getpid());
+        return 0;
+    } else if (pid > 0) {
+        printf("Parent process %u is running...\n", getpid());
+	printf("Polling forever...\n");
+        /* ring_buffer__poll: Poll for available data and consume records, 
+         * if any are available. Returns number of records consumed, or
+         * negative number, if any of the registered callbacks returned error. */
+        while (ring_buffer__poll(ringbuf, -1) >= 0);
+        /* If while loop is escapted, something must have gone wrong. */
+        fprintf(stderr, "ring buffer polling failed!\n");
+    } else {
+        printf("fork() failed!\n");
+	goto close_prog;
+    }
 
 close_prog:
-  bpf_camflow_kern__destroy(skel);
-  return 0;
+    bpf_camflow_kern__destroy(skel);
+    return 0;
 }
