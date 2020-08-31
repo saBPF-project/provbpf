b86c91be7756817cbe371449fed365c1bdc0347b
bstelea <bs17580@my.bristol.ac.uk>
2020-08-31 15:04:41 +0000

    add provenance record for current task

--- bpf_camflow_kern.c
+++ bpf_camflow_kern.c
@@ -106,7 +106,62 @@ static __always_inline void prov_update_task(struct task_struct *task,
 SEC("lsm/task_alloc")
 int BPF_PROG(task_alloc, struct task_struct *task, unsigned long clone_flags) {
     uint64_t key = get_key(task);
-    union prov_elt prov;
+    uint64_t current_task_key;
+    union prov_elt prov, prov_current_task;
+
+    // Record provenance for current task
+    __builtin_memset(&prov_current_task, 0, sizeof(union prov_elt));
+    prov_init(&prov_current_task, ACT_TASK);
+
+    struct task_struct *current_task = (struct task_struct *)bpf_get_current_task();
+
+    current_task_key = get_key(current_task);
+
+    // Dereference all the necessary fields from current_task
+    bpf_probe_read(&prov_current_task.task_info.pid, sizeof(prov_current_task.task_info.pid), &current_task->pid);
+    bpf_probe_read(&prov_current_task.task_info.vpid, sizeof(prov_current_task.task_info.vpid), &current_task->tgid);
+    bpf_probe_read(&prov_current_task.task_info.utime, sizeof(prov_current_task.task_info.utime), &current_task->utime);
+    bpf_probe_read(&prov_current_task.task_info.stime, sizeof(prov_current_task.task_info.stime), &current_task->stime);
+    struct mm_struct *mm;
+    bpf_probe_read(&mm, sizeof(mm), &current_task->mm);
+    bpf_probe_read(&prov_current_task.task_info.vm, sizeof(prov_current_task.task_info.vm), &mm->total_vm);
+    prov_current_task.task_info.vm = prov_current_task.task_info.vm * IOC_PAGE_SIZE / KB;
+    struct mm_rss_stat rss_stat;
+    bpf_probe_read(&rss_stat, sizeof(rss_stat), &mm->rss_stat);
+    prov_current_task.task_info.rss = (rss_stat.count[MM_FILEPAGES].counter +
+                                      rss_stat.count[MM_ANONPAGES].counter +
+                                      rss_stat.count[MM_SHMEMPAGES].counter) * IOC_PAGE_SIZE / KB;
+    uint64_t current_task_hw_vm, current_task_hw_rss;
+    bpf_probe_read(&current_task_hw_vm, sizeof(current_task_hw_vm), &mm->hiwater_vm);
+    prov_current_task.task_info.hw_vm = u64_max(current_task_hw_vm, prov_current_task.task_info.vm) * IOC_PAGE_SIZE / KB;
+    bpf_probe_read(&current_task_hw_rss, sizeof(current_task_hw_rss), &mm->hiwater_rss);
+    prov_current_task.task_info.hw_rss = u64_max(current_task_hw_rss, prov_current_task.task_info.rss) * IOC_PAGE_SIZE / KB;
+#ifdef CONFIG_TASK_IO_ACCOUNTING
+    bpf_probe_read(&prov_current_task.task_info.rbytes, sizeof(prov_current_task.task_info.rbytes), &current_task->ioac.read_bytes);
+    prov_current_task.task_info.rbytes &= KB_MASK;
+    bpf_probe_read(&prov_current_task.task_info.wbytes, sizeof(prov_current_task.task_info.wbytes), &current_task->ioac.write_bytes);
+    prov_current_task.task_info.wbytes &= KB_MASK;
+    bpf_probe_read(&prov_current_task.task_info.cancel_wbytes, sizeof(prov_current_task.task_info.cancel_wbytes), &current_task->ioac.cancelled_write_bytes);
+    prov_current_task.task_info.cancel_wbytes &= KB_MASK;
+#else
+    bpf_probe_read(&prov_current_task.task_info.rbytes, sizeof(prov_current_task.task_info.rbytes), &current_task->ioac.rchar);
+    prov_current_task.task_info.rbytes &= KB_MASK;
+    bpf_probe_read(&prov_current_task.task_info.wbytes, sizeof(prov_current_task.task_info.wbytes), &current_task->ioac.wchar);
+    prov_current_task.task_info.wbytes &= KB_MASK;
+    prov_current_task.task_info.cancel_wbytes = 0;
+#endif
+
+    union prov_elt *prov_val = bpf_map_lookup_elem(&task_map, &current_task_key);
+
+    if (prov_val) {
+      // If the current task is in the map, use the entry for provenance
+      record_provenance(prov_val);
+    } else {
+      // If the current task is not in the map, create an new entry in the map and use it for provenance
+      bpf_map_update_elem(&task_map, &current_task_key, &prov_current_task, BPF_NOEXIST);
+      record_provenance(&prov_current_task);
+    }
+
     __builtin_memset(&prov, 0, sizeof(union prov_elt)); // this is needed
 
     prov_init(&prov, ACT_TASK);
@@ -121,6 +176,7 @@ int BPF_PROG(task_alloc, struct task_struct *task, unsigned long clone_flags) {
 
     /* Record the provenance to the ring buffer */
     record_provenance(&prov);
+
     /* TODO: CODE HERE
      * Record provenance relations as the result of task allocation.
      */
