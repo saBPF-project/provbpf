3c95df87ae66503e1cbab779d820f8729d03f285
bstelea <bs17580@my.bristol.ac.uk>
2020-11-07 09:15:43 +0000

    implemented should_record_relation

--- bpf_camflow_kern.c
+++ bpf_camflow_kern.c
@@ -163,7 +163,7 @@ int BPF_PROG(task_fix_setgid, struct cred *new, const struct cred *old, int flag
       return 0;
     }
 
-    // generates(RL_SETGID, current_task, ptr_prov_old_cred, false, ptr_prov_task, false, ptr_prov_new_cred, false, NULL, flags);
+    generates(RL_SETGID, current_task, ptr_prov_old_cred, ptr_prov_task, ptr_prov_new_cred, NULL, flags);
 
     return 0;
 }
@@ -295,7 +295,7 @@ int BPF_PROG(inode_create, struct inode *dir, struct dentry *dentry, umode_t mod
       return 0;
     }
 
-    // generates(RL_INODE_CREATE, current_task, ptr_prov_current_cred, false, ptr_prov_current_task, false, ptr_prov_inode, false, NULL, mode);
+    generates(RL_INODE_CREATE, current_task, ptr_prov_current_cred, ptr_prov_current_task, ptr_prov_inode, NULL, mode);
 
     return 0;
 }
@@ -359,16 +359,22 @@ int BPF_PROG(inode_permission, struct inode *inode, int mask) {
       return 0;
     }
 
+    uint64_t relation_type = 0;
+
     // Left commented due to producing significant amount of output.
-    // if (mask & MAY_EXEC) {
-    //   uses(RL_PERM_EXEC, current_task, ptr_prov_inode, false, ptr_prov_current_task, false, ptr_prov_current_cred, false, NULL, mask);
-    // } else if (mask & MAY_READ) {
-    //   uses(RL_PERM_READ, current_task, ptr_prov_inode, false, ptr_prov_current_task, false, ptr_prov_current_cred, false, NULL, mask);
-    // } else if (mask & MAY_APPEND) {
-    //   uses(RL_PERM_APPEND, current_task, ptr_prov_inode, false, ptr_prov_current_task, false, ptr_prov_current_cred, false, NULL, mask);
-    // } else if (mask & MAY_WRITE) {
-    //   uses(RL_PERM_WRITE, current_task, ptr_prov_inode, false, ptr_prov_current_task, false, ptr_prov_current_cred, false, NULL, mask);
-    // }
+    if (mask & MAY_EXEC) {
+      relation_type = RL_PERM_EXEC;
+    } else if (mask & MAY_READ) {
+      relation_type = RL_PERM_READ;
+    } else if (mask & MAY_APPEND) {
+      relation_type = RL_PERM_APPEND;
+    } else if (mask & MAY_WRITE) {
+      relation_type = RL_PERM_WRITE;
+    }
+
+    if (relation_type != 0) {
+      uses(relation_type, current_task, ptr_prov_inode, false, ptr_prov_current_task, false, ptr_prov_current_cred, false, NULL, mask);
+    }
 
     return 0;
 }
@@ -416,7 +422,7 @@ int BPF_PROG(inode_link, struct dentry *old_dentry, struct inode *dir, struct de
       return 0;
     }
 
-    // generates(RL_LINK, current_task, ptr_prov_current_cred, false, ptr_prov_current_task, false, ptr_prov, false, NULL, 0);
+    generates(RL_LINK, current_task, ptr_prov_current_cred, ptr_prov_current_task, ptr_prov, NULL, 0);
 
     return 0;
 }
@@ -449,7 +455,7 @@ int BPF_PROG(inode_unlink, struct inode *dir, struct dentry *dentry) {
       return 0;
     }
 
-    // generates(RL_UNLINK, current_task, ptr_prov_current_cred, false, ptr_prov_current_task, false, ptr_prov, false, NULL, 0);
+    generates(RL_UNLINK, current_task, ptr_prov_current_cred, ptr_prov_current_task, ptr_prov, NULL, 0);
 
     return 0;
 }
@@ -483,7 +489,7 @@ int BPF_PROG(inode_symlink, struct inode *dir, struct dentry *dentry, const char
       return 0;
     }
 
-    // generates(RL_SYMLINK, current_task, ptr_prov_current_cred, false, ptr_prov_current_task, false, ptr_prov, false, NULL, 0);
+    generates(RL_SYMLINK, current_task, ptr_prov_current_cred, ptr_prov_current_task, ptr_prov, NULL, 0);
 
     return 0;
 }
@@ -520,7 +526,7 @@ int BPF_PROG(inode_rename, struct inode *old_dir, struct dentry *old_dentry, str
       return 0;
     }
 
-    // generates(RL_RENAME, current_task, ptr_prov_current_cred, false, ptr_prov_current_task, false, ptr_prov, false, NULL, 0);
+    generates(RL_RENAME, current_task, ptr_prov_current_cred, ptr_prov_current_task, ptr_prov, NULL, 0);
 
     return 0;
 }
@@ -573,7 +579,7 @@ int BPF_PROG(inode_setattr, struct dentry *dentry, struct iattr *attr) {
       return 0;
     }
 
-    // generates(RL_SETATTR, current_task, ptr_prov_current_cred, false, ptr_prov_current_task, false, ptr_prov_iattr, false, NULL, 0);
+    generates(RL_SETATTR, current_task, ptr_prov_current_cred, ptr_prov_current_task, ptr_prov_iattr, NULL, 0);
     derives(RL_SETATTR_INODE, ptr_prov_iattr, false, ptr_prov_inode, false, NULL, 0);
 
     return 0;
@@ -924,7 +930,7 @@ int BPF_PROG(cred_prepare, struct cred *new, const struct cred *old, gfp_t gfp) 
       return 0;
     }
 
-    // generates(RL_CLONE_MEM, current_task, ptr_prov_old, false, ptr_prov_task, false, ptr_prov_new, false, NULL, 0);
+    generates(RL_CLONE_MEM, current_task, ptr_prov_old, ptr_prov_task, ptr_prov_new, NULL, 0);
 
     return 0;
 }
